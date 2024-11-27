a0729af6df7702a909434a101d4393115146bf22
PrimalPimmy <prashant20.pm@gmail.com>
2024-11-27 16:45:13 +0530

    Initial changes
    Signed-off-by: PrimalPimmy <prashant20.pm@gmail.com>

--- main.go
+++ main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"path/filepath"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-	// Create in-cluster config
-	config, err := rest.InClusterConfig()
+	// // Create in-cluster config
+	// config, err := rest.InClusterConfig()
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
 	}
